0db8f5e84528414185935cd1f026723cccaf0584
enikeishik <enikeishik@gmail.com>
2019-06-18 14:44:01 +0300

    refactor: change single quotes to double

--- tir.js
+++ tir.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     var size = 12;
     var attempts = 50;
     var show_delay = 1200;
@@ -14,24 +14,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function getClientWidth()
     {
-        return document.compatMode==='CSS1Compat' && !window.opera?document.documentElement.clientWidth:document.body.clientWidth;
+        return document.compatMode==="CSS1Compat" && !window.opera?document.documentElement.clientWidth:document.body.clientWidth;
     }
 
     function getClientHeight()
     {
-        return document.compatMode==='CSS1Compat' && !window.opera?document.documentElement.clientHeight:document.body.clientHeight;
+        return document.compatMode==="CSS1Compat" && !window.opera?document.documentElement.clientHeight:document.body.clientHeight;
     }
     function getTitle()
     {
         var p = attempt_counter ? Math.round(catch_counter / attempt_counter * 100) : 0;
-        return 'Catch: ' + catch_counter + ', total: ' + attempt_counter + '/' + attempts + ', ' + p + '%';
+        return "Catch: " + catch_counter + ", total: " + attempt_counter + "/" + attempts + ", " + p + "%";
     }
     function Hide()
     {
-        point.style.display = 'none';
-        point.style.backgroundColor = '#cc0000';
-        point.style.width = size + 'px';
-        point.style.height = size + 'px';
+        point.style.display = "none";
+        point.style.backgroundColor = "#cc0000";
+        point.style.width = size + "px";
+        point.style.height = size + "px";
         if (attempt_counter >= attempts) {
             var s = getTitle();
             document.title = s;
@@ -46,8 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
         do {
             p_left = Math.round(Math.random() * 2000);
         } while ((p_left + size) > getClientWidth());
-        point.style.left = p_left + 'px';
-        point.style.top = p_top + 'px';
+        point.style.left = p_left + "px";
+        point.style.top = p_top + "px";
         var delay = max_hide_delay;
         do {
             delay = Math.round(Math.random() * max_hide_delay);
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function Show()
     {
         attempt_counter++;
-        point.style.display = 'block';
+        point.style.display = "block";
         timeout_id = setTimeout(Hide, show_delay);
     }
     function Catch()
@@ -66,19 +66,19 @@ document.addEventListener('DOMContentLoaded', function () {
         clearTimeout(timeout_id);
         catch_counter++;
         document.title = getTitle();
-        point.style.display = 'block';
-        point.style.backgroundColor = '#ff0000';
-        point.style.width = (size * 4) + 'px';
-        point.style.height = (size * 4) + 'px';
-        point.style.left = (parseInt(point.style.left, 10) - size) + 'px';
-        point.style.top = (parseInt(point.style.top, 10) - size) + 'px';
+        point.style.display = "block";
+        point.style.backgroundColor = "#ff0000";
+        point.style.width = (size * 4) + "px";
+        point.style.height = (size * 4) + "px";
+        point.style.left = (parseInt(point.style.left, 10) - size) + "px";
+        point.style.top = (parseInt(point.style.top, 10) - size) + "px";
         timeout_id = setTimeout(Hide, catch_delay);
     }
     function Init()
     {
-        alert('Lets begin');
-        point = document.getElementById('point');
-        point.addEventListener('click', Catch);
+        alert("Lets begin");
+        point = document.getElementById("point");
+        point.addEventListener("click", Catch);
         Hide();
     }
     
